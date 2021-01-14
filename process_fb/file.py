d6bc7407dbce535762cd2222919fbc052d1f0761
Hamish Gibbs <hamishgibbs@gmail.com>
2021-01-14 18:25:18 +0000

    return dict

--- process_fb/file.py
+++ process_fb/file.py
@@ -48,7 +48,9 @@ def watch_file_dates(in_path, out_path, to=date_to_date, extension='/*.csv', siz
 
         if len(new_files) == size:
 
-            file_groups.append(new_files)
+            new_date = to([utils.get_file_date(new_files[0])])[0]['new_date']
+
+            file_groups.append({'output_date': new_date, 'input_files': new_files})
 
     return(file_groups)
 
