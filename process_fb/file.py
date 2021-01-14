a2afbf88ffb8cd6b9f1cab6e4a78d0b19e8346b6
Hamish Gibbs <hamishgibbs@gmail.com>
2021-01-14 17:16:27 +0000

    process groups

--- process_fb/file.py
+++ process_fb/file.py
@@ -1,8 +1,22 @@
 import os
 import glob
+import collections
 from process_fb import utils
+from datetime import datetime
 
-def watch_files(in_path, out_path, to = None, extension = '/*.csv'):
+
+def date_to_date(dates: list):
+
+    ref_dates = []
+
+    for date in dates:
+
+        ref_dates.append({'new_date': date, 'orig_date': date})
+
+    return(ref_dates)
+
+
+def watch_file_dates(in_path, out_path, to=date_to_date, extension='/*.csv', size=1):
     # identify files in in directory that are not in out directory
 
     in_files = glob.glob(in_path + extension)
@@ -13,12 +27,55 @@ def watch_files(in_path, out_path, to = None, extension = '/*.csv'):
 
     out_file_dates = [utils.get_file_date(x) for x in out_files]
 
-    if to is None:
+    dates_transformed = to(list(in_file_dates))
 
-        difference = set(in_file_dates).difference(set(out_file_dates))
+    date_dict = collections.defaultdict(list)
 
-        match_index = [i for i, x in enumerate([x in in_file_dates for x in difference]) if x]
+    for d in dates_transformed:
+        date_dict[d['new_date']].append(d)
 
-        match_files = [in_files[i] for i in match_index]
+    date_groups = list(date_dict.values())
 
-    return(match_files)
+    file_groups = []
+
+    for group in date_groups:
+
+        in_file_dates_transformed = [x['new_date'] for x in group]
+
+        new_file_index = [i for i, x in enumerate(in_file_dates_transformed) if x not in out_file_dates]
+
+        new_files = [in_files[x] for x in new_file_index]
+
+        if len(new_files) == size:
+
+            file_groups.append(new_files)
+
+    return(file_groups)
+
+
+def date_to_daily(dates: list):
+
+    ref_dates = []
+
+    for date in dates:
+
+        ref_date = date.replace(hour=0, minute=0)
+
+        ref_dates.append({'new_date': ref_date, 'orig_date': date})
+
+    return(ref_dates)
+
+
+def date_to_weekly(dates: list):
+
+    ref_dates = []
+
+    for date in dates:
+
+        ref_week = date.strftime('%Y_%W') + '_1'
+
+        ref_date = datetime.strptime(ref_week, "%Y_%W_%w")
+
+        ref_dates.append({'new_date': ref_date, 'orig_date': date})
+
+    return(ref_dates)
--- tests/test_file.py
+++ tests/test_file.py
@@ -1,6 +1,8 @@
 import os
+import glob
 import pytest
 from process_fb import file
+from datetime import datetime
 
 
 @pytest.fixture(scope="session")
@@ -36,8 +38,113 @@ def test_watch_files_no_change(tmp_dir1, tmp_dir2):
 
         assert os.path.exists(path)
 
-    res = file.watch_files(tmp_dir1, tmp_dir2, to=None)
+    res = file.watch_file_dates(tmp_dir1, tmp_dir2, to=file.date_to_date, size = 1)
 
     assert type(res) is list
 
     assert len(res) == 1
+
+
+def test_watch_files_day_to_weekly(tmp_dir1, tmp_dir2):
+
+    files = glob.glob(tmp_dir1 + '/*')
+    for f in files:
+        os.remove(f)
+
+    files = glob.glob(tmp_dir2 + '/*')
+    for f in files:
+        os.remove(f)
+
+    fn = []
+
+    fn.append(tmp_dir1 + '/Britain_2020_03_09_0000.csv')
+    fn.append(tmp_dir1 + '/Britain_2020_03_10_0000.csv')
+    fn.append(tmp_dir1 + '/Britain_2020_03_11_0000.csv')
+    fn.append(tmp_dir1 + '/Britain_2020_03_12_0000.csv')
+    fn.append(tmp_dir1 + '/Britain_2020_03_13_0000.csv')
+    fn.append(tmp_dir1 + '/Britain_2020_03_14_0000.csv')
+    fn.append(tmp_dir1 + '/Britain_2020_03_15_0000.csv')
+
+    for path in fn:
+
+        with open(path, 'w') as f:
+
+            f.write('test')
+
+        assert os.path.exists(path)
+
+    res = file.watch_file_dates(tmp_dir1, tmp_dir2, to=file.date_to_weekly, size=7)
+
+    assert type(res) is list
+
+    assert len(res) == 1
+
+
+def test_watch_files_hrs_to_daily(tmp_dir1, tmp_dir2):
+
+    files = glob.glob(tmp_dir1 + '/*')
+    for f in files:
+        os.remove(f)
+
+    files = glob.glob(tmp_dir2 + '/*')
+    for f in files:
+        os.remove(f)
+
+    fn = []
+
+    fn.append(tmp_dir1 + '/Britain_2020_03_10_0000.csv')
+    fn.append(tmp_dir1 + '/Britain_2020_03_10_0800.csv')
+    fn.append(tmp_dir1 + '/Britain_2020_03_10_1600.csv')
+
+    for path in fn:
+
+        with open(path, 'w') as f:
+
+            f.write('test')
+
+        assert os.path.exists(path)
+
+    res = file.watch_file_dates(tmp_dir1, tmp_dir2, to=file.date_to_daily, size=3)
+
+    assert type(res) is list
+
+    assert len(res) == 1
+
+
+def test_date_to_date():
+
+    dates = [datetime(2020, 1, 1, 0),
+             datetime(2020, 1, 1, 8),
+             datetime(2020, 1, 1, 16)]
+
+    res = file.date_to_daily(dates)
+
+    assert type(res) is list
+    assert type(res[0]) is dict
+    assert res[0]['new_date'] == datetime(2020, 1, 1, 0)
+
+
+def test_date_to_daily():
+
+    dates = [datetime(2020, 1, 1, 0),
+             datetime(2020, 1, 1, 8),
+             datetime(2020, 1, 1, 16)]
+
+    res = file.date_to_daily(dates)
+
+    assert type(res) is list
+    assert type(res[0]) is dict
+    assert res[0]['new_date'] == datetime(2020, 1, 1)
+
+
+def test_date_to_weekly():
+
+    dates = [datetime(2020, 3, 10, 0),
+             datetime(2020, 3, 11, 0),
+             datetime(2020, 3, 12, 0)]
+
+    res = file.date_to_weekly(dates)
+
+    assert type(res) is list
+    assert type(res[0]) is dict
+    assert res[0]['new_date'] == datetime(2020, 3, 9)
